a43a1269b3c227809ef7014edd89927f0bc61f7a
johnmuthua <caveomuthama@gmail.com>
2021-01-18 18:39:50 +0300

    added reactstrap

--- testapi/src/components/header/Header.js
+++ testapi/src/components/header/Header.js
@@ -1,111 +1,13 @@
-import React from "react";
-
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
-    useParams
-} from "react-router-dom";
-
-import {
-    Navbar,
-    Nav,
-    NavDropdown,
-    Form,
-    FormControl,
-    Button
-} from "react-bootstrap";
-
-class Header extends React.Component {
+import React, { Component } from "react";
+import { Nav, Navbar } from "react-bootstrap";
+export default class Header extends Component {
     render() {
         return (
             <div>
-                <div className="row">
-                    <div className="col-md-12">
-                        <Router>
-                            <Navbar
-                                bg="dark"
-                                variant="dark"
-                                expand="lg"
-                                sticky="top"
-                            >
-                                <Navbar.Brand href="#home">
-                                    React Bootstrap Navbar
-                                </Navbar.Brand>
-
-                                <Navbar.Toggle aria-controls="basic-navbar-nav" />
-
-                                <Navbar.Collapse id="basic-navbar-nav">
-                                    <Nav className="mr-auto">
-                                        <Nav.Link href="/">Home</Nav.Link>
-
-                                        <Nav.Link href="/about-us">
-                                            Contact Us
-                                        </Nav.Link>
-
-                                        <Nav.Link href="/contact-us">
-                                            About Us
-                                        </Nav.Link>
-
-                                        <NavDropdown
-                                            title="Dropdown"
-                                            id="basic-nav-dropdown"
-                                        >
-                                            <NavDropdown.Item href="#action/3.1">
-                                                Action
-                                            </NavDropdown.Item>
-
-                                            <NavDropdown.Item href="#action/3.2">
-                                                Another action
-                                            </NavDropdown.Item>
-
-                                            <NavDropdown.Item href="#action/3.3">
-                                                Something
-                                            </NavDropdown.Item>
-
-                                            <NavDropdown.Divider />
-
-                                            <NavDropdown.Item href="#action/3.4">
-                                                Separated link
-                                            </NavDropdown.Item>
-                                        </NavDropdown>
-                                    </Nav>
-
-                                    <Form inline>
-                                        <FormControl
-                                            type="text"
-                                            placeholder="Search"
-                                            className="mr-sm-2"
-                                        />
-
-                                        <Button variant="outline-success">
-                                            Search
-                                        </Button>
-                                    </Form>
-                                </Navbar.Collapse>
-                            </Navbar>
-
-                            <br />
-
-                            <Switch>
-                                <Route exact path="/">
-                                    <Home />
-                                </Route>
-
-                                <Route path="/">
-                                    <AboutUs />
-                                </Route>
-
-                                <Route path="/">
-                                    <ContactUs />
-                                </Route>
-                            </Switch>
-                        </Router>
-                    </div>
-                </div>
+                <Navbar bg="dark" variant="dark" expand="sm" sticky="top">
+                    <Navbar.Brand>React Bootstrap Navbar</Navbar.Brand>
+                </Navbar>
             </div>
         );
     }
 }
-
-export default Header;
