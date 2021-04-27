2a86e1699b109211b3bb3b799a92dbfd8af82137
muthuajohn <caveomuthama@gmail.com>
2021-04-27 23:55:41 +0300

    feat:Added Cors middleware

---
+++ resources/js/api.js
@@ -0,0 +1,8 @@
+import axios from 'axios'
+import Axios from 'axios'
+
+
+const BASE_API_URL = 'http://localhost:8000/api';
+export const register = () => {
+    axios.post()
+}
--- routes/api.php
+++ routes/api.php
@@ -26,14 +26,14 @@ Route::group(['middleware' => ['auth:api']], function () {
 
 
 //public routes
-// Route::group([], function () {
-//     Route::get('profile{profile}', 'App\Http\Controllers\ProfileController@show');
-//     Route::get('/blog', 'App\Http\Controllers\BlogController@index');
-//     Route::get('/blog', 'App\Http\Controllers\BlogController@show');
-//     Route::post('/questions', 'App\Http\Controllers\QuestionController@index');
-//     Route::get('/questions/{question}', 'App\Http\Controllers\QuestionController@show');
-//     Route::get('answers{answers}', 'App\Http\Controllers\AnswersController@show');
-//     Route::post('/register', 'App\Http\Controllers\AccessController@register');
-//     Route::post('/login', 'App\Http\Controllers\AccessController@login');
-//     Route::get('/', 'App\Http\Controllers\BlogController@index')->name('home');
-// });
+Route::group([], function () {
+    Route::get('profile{profile}', 'App\Http\Controllers\ProfileController@show');
+    Route::get('/blog', 'App\Http\Controllers\BlogController@index');
+    Route::get('/blog', 'App\Http\Controllers\BlogController@show');
+    Route::post('/questions', 'App\Http\Controllers\QuestionController@index');
+    Route::get('/questions/{question}', 'App\Http\Controllers\QuestionController@show');
+    Route::get('answers{answers}', 'App\Http\Controllers\AnswersController@show');
+    Route::post('/register', 'App\Http\Controllers\AccessController@register');
+    Route::post('/login', 'App\Http\Controllers\AccessController@login');
+    Route::get('/', 'App\Http\Controllers\BlogController@index')->name('home');
+});
