19dd524d8179aace9f4b7b94f4a309633b22376a
muthuajohn <caveomuthama@gmail.com>
2021-04-29 15:49:05 +0300

    feat:created comments for answer and question

---
+++ database/factories/AnswerCommentFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\AnswerComment;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class AnswerCommentFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = AnswerComment::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition()
+    {
+        return [
+            //
+        ];
+    }
+}
